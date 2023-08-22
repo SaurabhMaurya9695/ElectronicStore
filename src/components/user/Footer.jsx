f800130f80c05d8d0eaf2199d6ca738d588aa4b8
Saurabh Maurya <sm0107870@gmail.com>
2023-08-22 10:50:16 +0530

    Working with axios

--- src/components/user/Footer.jsx
+++ src/components/user/Footer.jsx
@@ -1,11 +1,19 @@
+import axios from "axios";
 import { Button, Container } from "react-bootstrap";
 import { toast } from 'react-toastify';
 
 const Footer = ()=>{
 
-    function showToast(){
-        console.log("ok");
-        toast.success("this is toast");
+    function datafromServer(){
+        toast.info("this is toast");
+        let p = axios.get("http://localhost:2023/users");
+        p.then((response)=>{
+            console.log(response.data);
+            toast.success("Complete request");
+        }).catch((error)=>{
+            console.log("error");
+            toast.error("something went wrong");
+        })
     }
 
     return (
@@ -13,7 +21,7 @@ const Footer = ()=>{
         <Container fluid className="text-white  p-5 text-center bg-navbar-color" >
             <h3> We provides Best Products </h3>
             <h5>All rightsReserved : saurabhyash1707@gmail.com</h5>
-            <Button variant="success" onClick={showToast}></Button>
+            <Button variant="success" onClick={datafromServer}>Getting data from server</Button>
         </Container>
     );
 }
