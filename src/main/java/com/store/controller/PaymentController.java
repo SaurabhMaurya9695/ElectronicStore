fe976337c32a004451535643f4aba379472b7348
Saurabh Maurya <sm0107870@gmail.com>
2023-11-04 18:26:16 +0530

    Payment Working with notification

--- src/main/java/com/store/controller/PaymentController.java
+++ src/main/java/com/store/controller/PaymentController.java
@@ -2,11 +2,14 @@ package com.store.controller;
 
 import java.util.Map;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Hex;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +22,6 @@ import com.store.dto.UserDto;
 import com.store.entities.Order;
 import com.store.repository.OrderRepository;
 import com.store.responsemsg.ApiResponseMessage;
-import com.store.responsemsg.PaymentResponse;
-import com.store.service.OrderService;
 import com.store.service.UserService;
 
 @RestController
@@ -41,23 +42,15 @@ public class PaymentController {
 	private UserService userService;
 
 	@Autowired
-	private OrderService orderService;
-	
-	@Autowired
 	private OrderRepository orderRepository;
 
 	@PostMapping("/start")
-	public ResponseEntity<PaymentResponse> startPayment(@RequestBody Map<String, Object> data)
-			throws RazorpayException {
+	public String startPayment(@RequestBody Map<String, Object> data) throws RazorpayException {
 		System.out.println(data);
 		int amount = (int) data.get("amount");
 		String orderId = data.get("orderId").toString();
 		String userId = data.get("userId").toString();
-		
-		System.out.println(amount);
-		System.out.println(orderId);
-		System.out.println(userId);
-
+		this.sendNotifications(amount , userId , orderId);
 		// fetch user now
 		UserDto user = userService.getUserById(userId);
 
@@ -73,72 +66,49 @@ public class PaymentController {
 		JSONObject orderRequest = new JSONObject();
 		orderRequest.put("amount", amount * 100); // amount in the smallest currency unit
 		orderRequest.put("currency", RZP_CURRENCY);
-		orderRequest.put("reminder_enable" , true);
-		orderRequest.put("description" , "To Complete the Order Pay here !!");
+		orderRequest.put("receipt", user.getUserId());
+
 		
-		// creating Use
-		JSONObject customer = new JSONObject();
-		customer.put("name", user.getName());
-		customer.put("email", user.getEmail());
-		customer.put("userId", user.getUserId());
-		// we want to notify our user when we are doing payment
-		JSONObject notify = new JSONObject();
-		notify.put("sms", true);
-		notify.put("email", true);
-		
-		JSONObject notes = new JSONObject();
-		notes.put("policy_name","AapkiDukkan");
+		com.razorpay.Order createdOrder = client.orders.create(orderRequest);
+		createdOrder.get("id");
+		System.out.println(createdOrder);
+		return createdOrder.toString();
+	}
 
-		orderRequest.put("customer", customer);
-		orderRequest.put("notify", notify);
-		orderRequest.put("callback_url", "http://localhost:3000/users/payment-success/" + orderId);
-		orderRequest.put("callback_method", "get");
+	public static String encode(String key, String data) throws Exception {
+		Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+		SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+		sha256_HMAC.init(secret_key);
 
-		PaymentLink link = client.paymentLink.create(orderRequest);
-
-		System.out.println(link);
-		String paymentLinkId = link.get("id");
-		String paymentLinkUrl = link.get("short_url");
-
-		PaymentResponse response = new PaymentResponse();
-		response.setPayment_id(paymentLinkId);
-		response.setPayment_url(paymentLinkUrl);
-
-		Order order = orderRepository.findById(orderId).get();
-		System.out.println("order is");
-		System.out.println(order);
-		// creating order now
-//        Order createdOrder = client.orders.create(orderRequest) ;
-//        System.out.println(createdOrder);
-
-		// till now Order Created Successfully
-		// if you want to save this information to db then you can
-
-		return new ResponseEntity<PaymentResponse>(response, HttpStatus.CREATED);
+		return Hex.encodeHexString(sha256_HMAC.doFinal(data.getBytes("UTF-8")));
 	}
 
 	@GetMapping("/updateOrderAfterPayment")
-	public ApiResponseMessage updatePayment(@RequestParam("payment_id") String payment_Id , @RequestParam("orderId") String orderId) throws RazorpayException{
-		
+	public ApiResponseMessage updatePayment(@RequestParam("payment_id") String payment_Id,
+			@RequestParam("orderId") String orderId, @RequestParam("RZP_ORDER_ID") String RZP_ORDER_ID,
+			@RequestParam("signature") String signature 
+			) throws Exception {
+
 		Order order = null;
 		try {
 			order = orderRepository.findById(orderId).get();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("order is");
-		System.out.println(order.getOrderId());
-		System.out.println("till here " + order);
+		String generated_signature = encode(RZP_KEY_SECRET, RZP_ORDER_ID + "|" + payment_Id);
+		JSONObject options = new JSONObject();
+		options.put("razorpay_order_id", RZP_ORDER_ID);
+		options.put("razorpay_payment_id", payment_Id);
+		options.put("razorpay_signature", signature);
+
+		boolean status = Utils.verifyPaymentSignature(options, RZP_KEY_SECRET);
 		
-		RazorpayClient razorpay = new RazorpayClient(RZP_KEY_ID, RZP_KEY_SECRET);
-		
-		Payment payment = razorpay.payments.fetch(payment_Id);
-		if(payment.get("Status").equals("captured")) {
+		if (status) {
 			order.setOrderStatus("DISPATCHED");
 			order.setPayementStatus("PAID");
 			this.orderRepository.save(order);
 		}
-		
+
 		ApiResponseMessage message = new ApiResponseMessage();
 		message.setCode(HttpStatus.ACCEPTED);
 		message.setMessage("Payment Accepted SuccessFully");
@@ -146,4 +116,39 @@ public class PaymentController {
 		return message;
 	}
 	
+	public void sendNotifications (int amount , String userId , String orderID) throws RazorpayException {
+		RazorpayClient razorpay = new RazorpayClient(RZP_KEY_ID, RZP_KEY_SECRET);
+		JSONObject paymentLinkRequest = new JSONObject();
+		paymentLinkRequest.put("amount",amount * 100);
+		paymentLinkRequest.put("currency","INR");
+		paymentLinkRequest.put("description" , "To Complete the Order Pay here !! if this link is not working from mail then pay from website."
+				+ "and in UPI section use success@razorpay to make the transaction successfull");
+//		paymentLinkRequest.put("upi_link",true);
+		
+		UserDto user = userService.getUserById(userId);
+
+		JSONObject customer = new JSONObject();
+		customer.put("name", user.getName());
+		customer.put("email", user.getEmail());
+		customer.put("userId", user.getUserId());
+		paymentLinkRequest.put("customer",customer);
+		
+		JSONObject notify = new JSONObject();
+		notify.put("sms",true);
+		notify.put("email",true);
+		paymentLinkRequest.put("notify",notify);
+		paymentLinkRequest.put("reminder_enable",true);
+		
+		JSONObject notes = new JSONObject();
+		notes.put("policy_name","Aapki Dukaan");
+		paymentLinkRequest.put("notes",notes);
+		paymentLinkRequest.put("callback_url","http://localhost:3000/users/payment-success/" +orderID );
+		paymentLinkRequest.put("callback_method","get");
+		              
+		PaymentLink payment = razorpay.paymentLink.create(paymentLinkRequest);
+		System.out.println("send notification");
+		System.out.println(payment);
+		return ;
+	}
+
 }
